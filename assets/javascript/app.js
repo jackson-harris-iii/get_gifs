ce58619b09c4bddf477c77ddb662d6c694e42907
Apollo <jackson.harris3@gmail.com>
2018-02-23 02:08:45 -0800

    connect to GIFY API, add button request functionality, response logs to console

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,15 +2,32 @@ $(document).ready(function (){
 
 var gifs = ["49ers", "Lakers", "Giants", "Eagles", "Taylor Swift", "Kittens", "Rick and Morty", "Dragon Ball Z", "Anakin", "Brunch"]
 var search = null;
-var querlURL = "https://api.giphy.com/v1/gifs/search?api_key=j3QaK4ow9CkKDewMbh0mUJTYeLwPsnmG&q=" + search + "&limit=25&offset=0&rating=G&lang=en"
+
 
 function renderButtons() {
     $("#gifsView").empty()
     gifs.forEach(element => {
-        console.log(element)
-        $("#gifsView").append('<a href="#0" class="button" id="' + element + '">'+  element + '</a>')
+        $("#gifsView").append('<button class="button radius bordered shadow primary gifButton" id="' + element + '">'+  element + '</button>')
+
     });
 }
 
-renderButtons()
+function getGifs(request) {
+    
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=j3QaK4ow9CkKDewMbh0mUJTYeLwPsnmG&q=" + search + "&limit=25&offset=0&rating=G&lang=en"
+    search = request.target.id
+    console.log(search)
+    //call GIFY api and get GIFS broh.
+    $.ajax({
+        url: queryURL,
+        method: 'GET',
+    }).then(function (response) {
+        console.log(response)
+
+    })
+}
+
+renderButtons();
+$("body").on("click", ".gifButton", getGifs);
+
 })
