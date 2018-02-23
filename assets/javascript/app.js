6407a71b7bd63cd08fcec96d07c8647edeea6ff6
Apollo <jackson.harris3@gmail.com>
2018-02-23 03:32:55 -0800

    fix getGifs function to properly insert search into queryURL

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,6 @@ $(document).ready(function (){
    
 
 var gifs = ["49ers", "Lakers", "Giants", "Eagles", "Taylor Swift", "Kittens", "Rick and Morty", "Dragon Ball Z", "Anakin", "Brunch"]
-var search = null;
 
 
 function renderButtons() {
@@ -33,9 +32,13 @@ function newGif(addition) {
 
 function getGifs(request) {
     
+    //removes and GIFs currently in the GifBox div
+    $('#gifBox').empty()
+    var search = request.target.id
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=j3QaK4ow9CkKDewMbh0mUJTYeLwPsnmG&q=" + search + "&limit=10&offset=0&rating=G&lang=en"
-    search = request.target.id
+   
     console.log(search)
+    console.log(queryURL)
     //call GIFY api and get GIFS broh.
     $.ajax({
         url: queryURL,
